526683c00c2fed918bedebff5d66c464fdae8d4e
inaD12 <danstoy@abv.bg>
2026-04-29 14:17:17 +0300

    features/application: add FluentValidation and Mediatr

---
+++ ContactManager.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using ContactManager.Application.Features.PipelineBehaviors;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ContactManager.Application.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+	public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+	{
+		var currentAssembly = typeof(ServiceCollectionExtensions).Assembly;
+
+		services.AddMediatR(
+			cf =>
+			{
+				cf.RegisterServicesFromAssembly(currentAssembly);
+
+				cf.AddOpenBehavior(typeof(ValidationPipelineBehavior<,>));
+			});
+		
+		services
+			.AddValidatorsFromAssembly(currentAssembly);
+
+		return services;
+	}
+}
