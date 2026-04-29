57b182a866b25cc3964e90478493e847f4ac8aa2
inaD12 <danstoy@abv.bg>
2026-04-29 14:22:12 +0300

    features/application: add messaging abstractions

---
+++ ContactManager.Domain/Abstractions/Messaging/IQuery.cs
@@ -0,0 +1,8 @@
+﻿using ContactManager.Domain.Results;
+using MediatR;
+
+namespace ContactManager.Domain.Abstractions.Messaging;
+
+public interface IQuery<Tresponse> : IRequest<Result<Tresponse>>
+{
+}
---
+++ ContactManager.Domain/Abstractions/Messaging/IQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using ContactManager.Domain.Results;
+using MediatR;
+
+namespace ContactManager.Domain.Abstractions.Messaging;
+
+public interface IQueryHandler<TQuery, TResponse>
+	: IRequestHandler<TQuery, Result<TResponse>>
+	where TQuery : IQuery<TResponse>
+{
+}
