eb33557e12b869445a0897a9399fb4ebf5e4ec3d
inaD12 <danstoy@abv.bg>
2026-04-30 12:36:42 +0300

    features/application: add GetContactById functionality

---
+++ ContactManager.Application/Features/Queries/GetContactById/GetContactByIdQueryValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace ContactManager.Application.Features.Queries.GetContactById;
+
+public class GetContactByIdQueryValidator : AbstractValidator<GetContactByIdQuery>
+{
+	public GetContactByIdQueryValidator()
+	{
+		RuleFor(x => x.Id)
+			.NotEmpty()
+			.Must(BeAValidGuid)
+			.WithMessage("Id must be a valid GUID.");
+	}
+
+	private bool BeAValidGuid(string contactId)
+	{
+		return Guid.TryParse(contactId, out _);
+	}
+}
