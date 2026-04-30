eb25e1872303dbe601f34a6863221750fc7a5954
inaD12 <danstoy@abv.bg>
2026-04-30 15:38:53 +0300

    features/api: setup Program.cs

--- ContactManager.API/Program.cs
+++ ContactManager.API/Program.cs
@@ -1,9 +1,24 @@
+using ContactManager.Application.Extensions;
+using ContactManager.Extensions;
+using ContactManager.Features.Helpers;
+using ContactManager.Features.Utilities;
+using ContactManager.Infrastructure.Extensions;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var config = builder.Configuration;
+
+builder.Host.ConfigureSerilog();
+
+builder.Services
+    .AddApplicationLayer()
+    .AddInfrastructureLayer(config)
+    .AddAPILayer(config);
 
 var app = builder.Build();
+
+await app.SetUpDatabaseAsync();
 
 if (app.Environment.IsDevelopment())
 {
@@ -11,7 +26,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseExceptionHandler();
+
+app.UseSerilogRequestLogging();
+
+app.UseCors(AppPolicies.CorsPolicy);
+
+EndpointMapper.MapAllEndpoints(app);
 
 app.Run();
-
